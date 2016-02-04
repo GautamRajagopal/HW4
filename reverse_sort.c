df8a7a0e9121433659842e77c59f9373e3372a23
GautamRajagopal <rgautam@umich.edu>
2016-02-04 07:12:06 -0500

    Update reverse_sort.c

--- reverse_sort.c
+++ reverse_sort.c
@@ -1,1 +1,16 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#include "sort.h"
+
+
+int compare(int a, int b) {
+	    return b - a;
+}
+
+int cmpfunk (const void *a, const void *b)
+{
+	return ( *(int*)b - *(int*)a );
+}
